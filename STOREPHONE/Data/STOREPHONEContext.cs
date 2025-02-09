375ed0c3b502cd5620022c3b45d4ab94bdd5bd7c
ADRIAN ROMERO DE OLEO <adrianromerodeoleo27@gmail.com>
2025-02-08 21:21:59 -0400

    Tara2-CRU-Storephone Realizada

---
+++ STOREPHONE/Data/STOREPHONEContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using STOREPHONE.Models;
+
+namespace STOREPHONE.Data
+{
+    public class STOREPHONEContext : DbContext
+    {
+        public STOREPHONEContext (DbContextOptions<STOREPHONEContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<STOREPHONE.Models.phone> phone { get; set; } = default!;
+    }
+}
---
+++ STOREPHONE/Models/phone.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace STOREPHONE.Models
+{
+
+    [Table("telefonos")]
+    public class phone
+    {
+    
+        
+         public int Id { get; set; }
+         public string Marca { get; set; }
+         public string Color { get; set; }
+         public int Cantidad { get; set; }
+         public decimal Costo { get; set; }
+        
+         
+
+    }
+}
